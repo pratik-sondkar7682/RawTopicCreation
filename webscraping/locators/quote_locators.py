dc943d82ac0bb72534862a0c0cc3bb524fe67027
pratik-sondkar7682 <134318570+pratik-sondkar7682@users.noreply.github.com>
2023-05-24 23:38:39 +0530

    Delete webscraping directory

--- webscraping/locators/quote_locators.py
+++
@@ -1,4 +0,0 @@
-class QuoteLocators:
-    CONTENT = 'span.text'
-    AUTHOR = 'small.author'
-    TAG = 'div.tags a.tag'
--- webscraping/locators/quote_page_locators.py
+++
@@ -1,5 +0,0 @@
-class QuotePageLocator:
-    QUOTE = 'div.quote'
-    AUTHOR_DROPDOWN = 'select#author'
-    TAG_DROPDOWN = 'select#tag'
-    SEARCH_BUTTON = 'input[name="submit_button"]'
